6165ba9df6f5a87f80672cbcaccc052b18a2e3ac
Yuri Santos <yurifds.eng@gmail.com>
2022-11-13 23:05:17 -0400

    feat!(guess_num_machine): added guess mode for cpu
    Implemented mode choice.
    
    Changed the text on try agains for human players.
    
    Added rules on readme file.

---
+++ Guess_Num/README.md
@@ -0,0 +1,8 @@
+Welcome to Guess a Number!!
+
+This little game functions as follows:
+
+* You can either be the guesser [1] or the one who picked the number to be guessed [2].
+* If you are the guesser, the script will ask you to guess a number between 1 and 25. If the guess is too low or too high, you'll be prompted to try again! You have 3 chances to guess the correct number.
+* If you want to pick the number, you'll be prompted to enter the maximum number. Then, the scrpit will try to guess which number you picked, with 9 tries. You can either tell it if the number you picked is lesser then [L], higher than [H] or if it's the correct number [C].
+* The game ends if either you or the script guess correctly or your chances run out.
--- Guess_Num/main.py
+++ Guess_Num/main.py
@@ -13,21 +13,46 @@ def guess_num_hum(x):
             guess_num = int(input(f'Please, guess a number between 1 and {max_num}: '))
             first_guess = False
         else:
-            guess_num = int(input("Next try: "))
+            guess_num = int(input("Guess again: "))
 
         if guess_num < ramd_num:
-            print(f'{guess_num} is lower than the picked number. Guess again!')
+            print(f'{guess_num} is lower than the picked number!')
             chances -= 1
         elif guess_num > ramd_num:
-            print(f'{guess_num} is higher than the picked number. Guess again!')
+            print(f'{guess_num} is higher than the picked number!')
             chances -= 1
 
     if chances == 0:
         print(f"Sorry, your chances ran out! The correct number was {ramd_num}")
     else:
-    print(f"You're right! The number picked was {ramd_num}")
+        print(f"You're right! The number picked was {ramd_num}")
 
 
+def guess_num_machine():
+    min_num = 1
+    max_num = int(input("Please, input the maximum number the scrpit can guess: "))
+    chances = 9
+    reply = ''
+    while reply != 'c' and chances > 0:
+        print(f"The script has {chances} chances left")
+        guess = ramd.randint(min_num, max_num)
+        reply = input(f"The script guessed {guess}. Is the guess [L]ower, [H]igher or [C]orrect than the picked number?: ").lower()
+        if reply == 'l':
+            min_num = guess + 1
+            chances -= 1
+        elif reply == 'h':
+            max_num = guess - 1
+            chances -= 1
+
+    if chances == 0:
+        print("You won! The script couldn't guess your number!")
+    else:
+        print(f"The script guessed right! The number was {guess}!")
 
 if __name__ == '__main__':
-    guess_num_hum(10)
+    
+    mode = int(input("[1] is for you to guess and [2] the script will try to guess. Please choose the mode: "))
+    if mode == 1:
+        guess_num_hum(25)
+    elif mode == 2:
+        guess_num_machine()
